cfd11fc9fb2f55b52fa3f60286825036c91433b8
Binod Sharma <thebinod404@gmail.com>
2026-07-01 19:19:32 +0545

    Used the zip file instead of raw csv file

--- fastapi-backend/data/dataset.py
+++ fastapi-backend/data/dataset.py
@@ -1,14 +1,6 @@
 import pandas as pd
 import os
-import gdown
 
-FILE_ID = "16HBfourvU0XTMUqfiHD9uuoLnc5XQFpm"
 
-if not os.path.exists("ipl.csv"):
-    gdown.download(
-        f"https://drive.google.com/uc?id={FILE_ID}",
-        "ipl.csv",
-        quiet=False
-    )
 
-data = pd.read_csv("ipl.csv", low_memory=False)
+data = pd.read_csv("datasets/ipl.csv.gz", low_memory=False , compression='gzip')
